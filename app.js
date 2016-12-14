292bb9a951b993eb99f1354bdd4b9cd4a0053689
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2016-12-14 14:59:13 +1300

    Untested setup to insert data into DB and render on profilePage

--- app.js
+++ app.js
@@ -14,46 +14,63 @@ var knex = Knex(knexConfig)
 module.exports = app;
 
 //Handlebars engine
-app.engine('handlebars', expresshbs({defaultLayout: 'main'}))
+app.engine('handlebars', expresshbs({
+    defaultLayout: 'main'
+}))
 app.set('view engine', 'handlebars')
 
 //Body-parser makes a JSON body object out of request
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/", function(req, res){
-  res.redirect("/home")
+app.get("/", function(req, res) {
+    res.redirect("/home")
 })
 
 app.get("/home", function(req, res) {
-  res.render("home")
+    res.render("home")
 })
 
 app.post("/home/result", function(req, res) {
-  var search = req.body
-  imdb.getReq(search, function(err, data){
-    if (err) {res.redirect("/home/error")}
-    var movieObject = data
-    var resultObject = _.pick(movieObject, ["title", "poster", "plot", "rating"])
-  res.render("moviePage", resultObject)
-  })
+    var search = req.body
+    imdb.getReq(search, function(err, data) {
+        if (err) {
+            res.redirect("/home/error")
+        }
+        var movieObject = data
+        var resultObject = _.pick(movieObject, ["title", "poster", "plot", "rating"])
+        res.render("moviePage", resultObject)
+    })
 })
 
-app.get("/home/error", function(req, res){
-  res.send("Not found :|")
+app.get("/home/error", function(req, res) {
+    res.send("Not found :|")
 })
 
 app.post("/home/comparison", function(req, res) {
-  var myRating = Number(req.body.myRating)
-  var imdbRating = Number(req.body.imdbRating)
+    var myRating = Number(req.body.myRating)
+    var imdbRating = Number(req.body.imdbRating)
 
-  if (myRating > imdbRating + 1 ) {
-    res.render("greenComparisonPage", req.body)
-  }
-  else if (myRating < imdbRating - 1) {
-    res.render("redComparisonPage", req.body)
-  }
-  else res.render("greyComparisonPage", req.body)
+    if (myRating > imdbRating + 1) {
+        res.render("greenComparisonPage", req.body)
+    } else if (myRating < imdbRating - 1) {
+        res.render("redComparisonPage", req.body)
+    } else res.render("greyComparisonPage", req.body)
+})
 
+app.post("/home/profile", function(req, res) {
+    var movieRating = req.body
+    knex("profile")
+        .insert(movieRating)
+        .select("*")
+        .then(movieRatingArray => {
+            var data = {
+                profile: movieRatingArray
+            }
+            res.render("userProfile", data)
+                .catch(error => console.log(error))
+        })
 })
